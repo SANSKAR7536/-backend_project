b89e22ddf605495e114c332f817600561e23f0a9
sanskar kumar <sanskarkumar7536@gmail.com>
2025-09-12 14:33:55 +0530

    writing the method and jwt

---
+++ src/model/uer.model.js
@@ -0,0 +1,114 @@
+ import { jwt } from "jsonwebtoken";
+ import bcrypt from "bcrypt"
+import mongoose,{Schema} from "mongoose";
+
+
+ const UserSchema=Schema({
+    id:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+    },
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        max:50,
+        min:12,
+        trim:true,  // remove white spaces
+
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        unique:true,
+
+    },
+    fullName:{
+        type:String,
+        required:true,
+        lowercase:true,
+        trim:true,
+        index:true    // if a search query is applied then use this
+
+    },
+    avatar: {
+        type:String,
+        required:true,
+       
+
+    },
+    coverImage:{
+        type:String,
+        required:true,
+        
+    },
+    password:{
+        type:String,
+        required:[true,"Password is Required"]
+    },
+    refreshToke:{
+        type:String,
+
+    },
+    createdAt:{
+        type:Date,
+        required:true,
+
+    },
+    updatedAt:{
+        type:Date,
+        required:true,
+    },
+    watchHistory:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video",
+    }
+
+
+ },{timestamps:true})
+ UserSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();   // save  eacch time when only get updated not wen user changes any other thing
+    this.password=await bcrypt.hash(this.password,10)
+    next()
+
+ })
+  UserSchema.methods.isPasswordCorrect=async function (password){
+    return await bcrypt.compare(password,this.password)
+
+  }
+   UserSchema.methods.generateAceesToken=function (){
+    return jwt.sign(
+       {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET
+        ,{
+            expiresIn:ACCESS_TOKEN_EXPIRY
+        }
+
+
+    )
+   }
+   UserSchema.methods.generateRefreshToken=function (){
+    return jwt.sign(
+       {
+            _id: this._id,
+           
+        },
+        process.env.REFRESH_TOKEN_SECRET
+        ,{
+            expiresIn:REFRESH_TOKEN_EXPIRY
+        }
+
+
+    )
+   }
+
+ const User=mongoose.model("User",UserSchema);
---
+++ src/model/video.model.js
@@ -0,0 +1,66 @@
+
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+mongooseAggregatePaginate
+ const VideoSchema= Schema({
+    videoFile:{
+        type:String,   // by claudinary 
+        required:true,
+        
+    },
+    thumbnail:{       
+        type:String,
+        required:true,
+
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    },
+    title:{
+        type:String,
+        required:[true,"TITLE IS REQUIRED"],
+        trim:true,
+        lowercase:true,
+        min:12,
+        max:100,
+
+    },
+    description:{
+        type:String,
+        trim:true,
+        lowercase:true,
+        max:120,
+
+    },
+    duration:{
+        type:Number,  //  DURATION IS GIVE BY CLAUDINARY NOT BY US 
+
+    },
+    views:{
+        type:Number,
+        default:0
+
+    },
+    isPublished:{
+        type:Boolean,
+        default:true
+
+    },
+    createdAt:{
+        type:Date,
+    },
+    updatedAt:{
+        type:Date,
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user"
+    }
+
+
+
+ },{timestamps:true})
+ VideoSchema.plugin(mongooseAggregatePaginate)
+
+ export const Video=mongoose.model("Video",VideoSchema)
